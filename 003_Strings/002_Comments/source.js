1ae93abcd602ea977aa740bda49fb12e0cbe805b
kruckenberg <nkruckenberg@gmail.com>
2024-10-20 07:29:37 -0500

    delete outdated

--- 003_Strings/002_Comments/source.js
+++
@@ -1,3 +0,0 @@
-// The JavaScript engine ignores lines that begin with //
-// To run the code on the line below, delete the //
-// console.log("Hello, World!");
--- 003_Strings/003_Testing_Testing/source.js
+++
@@ -1,1 +0,0 @@
-let smallestPerfectNumber = 6;
